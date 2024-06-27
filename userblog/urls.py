4e7ac1e2740f50ff792ab77e0dd0ea755e76b6b2
Dheerajvishwakarma2004 <dheerajvishwakarma426@gmail.com>
2024-06-27 09:59:21 +0530

    create post added

--- userblog/urls.py
+++ userblog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, BlogDetail
+from .views import HomeView, BlogDetail , create_post
 
 urlpatterns = [
     path('', HomeView.as_view(), name = "home"),
     path('article/<int:pk>', BlogDetail.as_view(), name = "blog-detail"),
+    path('create/', create_post, name='create_post'),
 ]
--- userblog/views.py
+++ userblog/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from .models import Post 
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class HomeView(ListView):
     model = Post
-    template_name = "home.html"
+    template_name = 'home.html'
+    context_object_name = 'object_list'
 
 class BlogDetail(DetailView):
     model = Post
-    template_name = "blogdetail.html"
+    template_name = 'blogdetail.html'
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+        else:
+            print(form.errors) 
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
